5447a51ace37dd0a2d959a485aa1b8d238c67a46
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-04-27 16:15:47 +0200

    Change html, adjust css and add JS

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const indexPage = document.querySelector('.index-page')
+const bookmarkPage = document.querySelector('.bookmark-page')
+const formPage = document.querySelector('.form-page')
+const profilePage = document.querySelector('.profile-page')
+
+const buttonIndex = document.querySelector('.button-index')
+const buttonBookmark = document.querySelector('.button-bookmark')
+const buttonForm = document.querySelector('.button-form')
+const buttonProfile = document.querySelector('.button-profile')
+
+buttonIndex.addEventListener('click', () => {
+  indexPage.classList.remove('hidden')
+  bookmarkPage.classList.add('hidden')
+  formPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonIndex.classList.add('active')
+  buttonBookmark.classList.remove('active')
+  buttonForm.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+buttonBookmark.addEventListener('click', () => {
+  indexPage.classList.add('hidden')
+  bookmarkPage.classList.remove('hidden')
+  formPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonIndex.classList.remove('active')
+  buttonBookmark.classList.add('active')
+  buttonForm.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+buttonForm.addEventListener('click', () => {
+  indexPage.classList.add('hidden')
+  bookmarkPage.classList.add('hidden')
+  formPage.classList.remove('hidden')
+  profilePage.classList.add('hidden')
+
+  buttonIndex.classList.remove('active')
+  buttonBookmark.classList.remove('active')
+  buttonForm.classList.add('active')
+  buttonProfile.classList.remove('active')
+})
+buttonProfile.addEventListener('click', () => {
+  indexPage.classList.add('hidden')
+  bookmarkPage.classList.add('hidden')
+  formPage.classList.add('hidden')
+  profilePage.classList.remove('hidden')
+
+  buttonIndex.classList.remove('active')
+  buttonBookmark.classList.remove('active')
+  buttonForm.classList.remove('active')
+  buttonProfile.classList.add('active')
+})
